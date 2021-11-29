0ce0f724be60fe43335c6216ca719a180976812f
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-11-29 17:45:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ Main functions to implemented:
 3. void slide(int tiles[4][4], int &score); //Read user's input of direction, slide the tiles, and update the score. Supported by:
   (i)   void move(int tiles[4][4], char direction); //Move the tiles according to the direction.
   (ii)  int merge(int tiles[4][4], char direction); //Merge two or more tiles according to the direction, if possible.
-5. bool fail(int tiles[4][4]); //Check if the game should end (when there's no legal moves)
+4. bool fail(int tiles[4][4]); //Check if the game should end (when there's no legal moves)
 */
 
 //main.cpp
@@ -14,6 +14,8 @@ Main functions to implemented:
 //#include ".h"
 using namespace std;
 
+int pause = 0;
+int exit = 0;
 int main(){
   int tiles[4][4];
   int score = 0;
@@ -21,14 +23,25 @@ int main(){
   Some personalised functions here.
   Print the instructions as well.
   */
-  random(tiles);
-  random(tiles);
-  printTiles(tiles, score);
-  While(!fail(tiles)){
-    slide(tiles, &score);
-    printTiles(tiles, score);
+  while (!exit){
+    random(tiles);
     random(tiles);
     printTiles(tiles, score);
+    while(!fail(tiles)){
+      slide(tiles, &score);
+      printTiles(tiles, score);
+      random(tiles);
+      printTiles(tiles, score);
+    }
+    char answer;
+    cout << "Do you want to exit the game? (Y/N)" << endl;
+    cin >> answer;
+    if (answer == 'Y')
+      break;
+    else{
+      cout << "Game resumes." << endl;
+      printTiles(tiles, score);
+    }
   }
 //Saving the file and etc.
 }
