b3517d1fe00e8050d595b0399b1de9fe93f5c6a7
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-11-29 21:05:19 +0800

    Update main.cpp
    Add feature of Pause.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ Data Structure for storing infomation in the 16 tiles: a 4*4 int array.
 Main functions to implemented:
 1. void printTiles(int tiles[4][4], int score); //Print the interface of the game.
 2. void random(int tiles[4][4]); //Generate one random tile of 2 or 4 in the blank space.
-3. void slide(int tiles[4][4], int &score); //Read user's input of direction, slide the tiles, and update the score. Supported by:
+3. bool slide(int tiles[4][4], int &score); //Read user's input of direction, slide the tiles, and update the score. Supported by:
   (i)   void move(int tiles[4][4], char direction); //Move the tiles according to the direction.
   (ii)  int merge(int tiles[4][4], char direction); //Merge two or more tiles according to the direction, if possible.
 4. bool fail(int tiles[4][4]); //Check if the game should end (when there's no legal moves)
@@ -19,12 +19,11 @@ Main functions to implemented:
 //#include ".h"
 using namespace std;
 
-int pause = 0;
-int exit = 0;
 int main(){
+  int exit = 0;
   int tiles[4][4];
   int score = 0;
-  string intro = "Please select an option to continue:\n1: How to play?\n2: Start a new game.\n3: Load saved game.\n"
+  string intro = "Please select an option to continue:\n1: How to play?\n2: Start a new game.\n3: Load saved game.\n4: exit.\n"
   cout << "Welcome to 2048!" << endl;
   cout << intro;
   char choice;
@@ -46,6 +45,8 @@ int main(){
       loadGame(tiles, &score);
       break;
     }
+    else if (choice == '4')
+      exit = 1;
     else{
       cout << "Invalid choice. Please input again." << endl;
     }
@@ -56,21 +57,35 @@ int main(){
     random(tiles);
     printTiles(tiles, score);
     while(!fail(tiles)){
-      slide(tiles, &score);
-      printTiles(tiles, &score);
-      random(tiles);
-      printTiles(tiles, &score);
+      if (slide(tiles, &score)){
+        printTiles(tiles, &score);
+        random(tiles);
+        printTiles(tiles, &score);
+        continue;
+      }
+      else{
+        cout << "Game paused. Do you want to exit the game? (Y/*)" << endl;
+        char resume;
+        cin >> resume;
+        if (resume == 'Y' || resume == 'y'){
+          char save;
+          cout << "Do you want to save the game? (Y/*)" << endl;
+          if (save == 'Y' || save == 'y'){
+            saveGame(tiles, score);
+          }
+          break;
+        }
+        else{
+        cout << "Game resumes." << endl;
+        printTiles(tiles, score);
+        }
+      }
     }
-
-    char answer;
-    cout << "Do you want to exit the game? (Y/N)" << endl;
-    cin >> answer;
-    if (answer == 'Y')
+    char replay;
+    cout << "Game ends. Play again? (Y/*)" << endl;
+    cin >> replay;
+    if (replay != 'Y' && replay != 'y')
       break;
-    else{
-      cout << "Game resumes." << endl;
-      printTiles(tiles, score);
-    }
   }
-//Saving the file and etc.
+  cout << "Bye!" << endl;
 }
