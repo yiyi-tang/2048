d0208c3bfb34532d51db7152d2930151bd252a79
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-11-29 20:04:47 +0800

    Update main.cpp
    Guiding conversation added.

--- main.cpp
+++ main.cpp
@@ -7,10 +7,15 @@ Main functions to implemented:
   (i)   void move(int tiles[4][4], char direction); //Move the tiles according to the direction.
   (ii)  int merge(int tiles[4][4], char direction); //Merge two or more tiles according to the direction, if possible.
 4. bool fail(int tiles[4][4]); //Check if the game should end (when there's no legal moves)
+5. void loadGame(int tiles[4][4], int &score) and void saveGame(int tiles[4][4], int &score);
+//For loading and saving game status.
 */
 
 //main.cpp
 #include <iostream>
+#include <ifstream>
+#include <ofstream>
+#include <string>
 //#include ".h"
 using namespace std;
 
@@ -19,20 +24,44 @@ int exit = 0;
 int main(){
   int tiles[4][4];
   int score = 0;
-  /*
-  Some personalised functions here.
-  Print the instructions as well.
-  */
+  string intro = "Please select an option to continue:\n1: How to play?\n2: Start a new game.\n3: Load saved game.\n"
+  cout << "Welcome to 2048!" << endl;
+  cout << intro;
+  char choice;
+  while (cin >> choice){
+    if (choice == '1'){
+      ifstream fin;
+      string line;
+      fin.open("rules.txt");
+      if ( fin.fail() ){
+        cout << "Error in file opening!" << endl;
+      }
+      while ( getline(fin, line) ){
+        cout << line << endl;
+      }
+    }
+    else if (choice == '2')
+      break;
+    else if (choice == '3'){
+      loadGame(tiles, &score);
+      break;
+    }
+    else{
+      cout << "Invalid choice. Please input again." << endl;
+    }
+    cout << intro;
+  }
   while (!exit){
     random(tiles);
     random(tiles);
     printTiles(tiles, score);
     while(!fail(tiles)){
       slide(tiles, &score);
-      printTiles(tiles, score);
+      printTiles(tiles, &score);
       random(tiles);
-      printTiles(tiles, score);
+      printTiles(tiles, &score);
     }
+
     char answer;
     cout << "Do you want to exit the game? (Y/N)" << endl;
     cin >> answer;
