62576676af5f982359b880861ffb62a0d4e8f194
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-11-29 14:30:00 +0800

    Update main.cpp
    Correct the function prototypes and descriptions.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 /*
 Data Structure for storing infomation in the 16 tiles: a 4*4 int array.
 Main functions to implemented:
-1. void printTiles(int tiles[][], int score); //Print the interface of the game.
-2. void random(int tiles[][]); //Generate one random tile of 2 or 4 in the blank space.
-3. void slide(int tiles[][], int &score); //Read user input and slide the tiles.
-4. int merge(int tiles[][]); //Merge two or more tiles if possible and return the incresed score.
-5. bool fail(int tiles[][]); //Check if the game should end (when there's no legal moves)
+1. void printTiles(int tiles[4][4], int score); //Print the interface of the game.
+2. void random(int tiles[4][4]); //Generate one random tile of 2 or 4 in the blank space.
+3. void slide(int tiles[4][4], int &score); //Read user's input of direction, slide the tiles, and update the score. Supported by:
+  (i)   void move(int tiles[4][4], char direction); //Move the tiles according to the direction.
+  (ii)  int merge(int tiles[4][4], char direction); //Merge two or more tiles according to the direction, if possible.
+5. bool fail(int tiles[4][4]); //Check if the game should end (when there's no legal moves)
 */
 
 //main.cpp
