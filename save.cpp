121ba2ddf58116eb5126c41fe27135d5d4f34f16
sdfvj <91207470+sdfvj@users.noreply.github.com>
2021-11-30 22:08:55 +0800

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -10,12 +10,12 @@ void save(int chessboard[4][4])
     ofstream outfile;
 	outfile.open("memory.txt", ios::in);
 
-	for (int i = 0; i < 4; i++)
+   for (int i = 0; i < 4; i++)
    {
       for (int j=0; j<4; j++) {
-		outfile << chessboard[i][j] << "  ";}
+	outfile << chessboard[i][j] << "  ";}
         
-	}
+    }
 
 	outfile.close();//关闭文件，保存文件。
     return 0;
