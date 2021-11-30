3ae1079fe3195838297d8be34f19ad63e7e10994
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-11-30 11:26:51 +0800

    Update over.cpp

--- over.cpp
+++ over.cpp
@@ -1,50 +1,30 @@
-bool over()
+bool fail(int chessboard[4][4])
 {
-	product_number();
-	//if the chessboard is full
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{if (chessboard[i][j] == 0)
-				return true;}
+	//if the chessboard is not full
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++){
+			if (chessboard[i][j] == 0)
+				return false;
+		}
 	}
 	
 	//if there is still something we can combine
-	//1st line
-	for (int i = 0, j = 0; j <3; j++){
-		if (chessboard[i][j] == chessboard[i][j+1])
-			return true;
+	//lines
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 3; j++){
+			if (chessboard[i][j] == chessboard[i][j+1])
+				return false;
+		}
 	}
 	
 	
-	//last line
-	for (int i =3, j = 0; j < 3; j++){
-		if (chessboard[i][j] == chessboard[i][j+1])
-			return true;
+	//columns
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 3; j++){
+			if (chessboard[j][i] == chessboard[j+1][i])
+				return false;
+		}
 	}
 	
-	
-	//first column
-	for (int i = 0, j = 0; j < 3; j++){
-		if (chessboard[j][i] == chessboard[j+1][i])
-			return true;
-	}
-	
-	
-	//last column
-	for (int i =3, j = 0; j < 3; j++){
-		if (chessboard[j][i] == chessboard[j+1][i])
-			return true;
-	}
-	
-	
-	//the central elements
-	for (int i = 1; i < 3; i++)
-	{
-		for (int j = 1; j < 3; j++){
-			if (chessboard[i][j] == chessboard[i][j + 1] || chessboard[i][j] == chessboard[i][j - 1] || chessboard[i][j] == chessboard[i + 1][j] || chessboard[i][j] == chessboard[i - 1][j])
-				return true;}
-	return false;
-	}
+	return true;
 }
-//咱有成功判定了不
