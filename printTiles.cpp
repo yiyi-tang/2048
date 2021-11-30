1b5fbcecd995ad0fa582cd3078f4a69dbb1964c1
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-11-30 12:04:15 +0800

    Update printTiles.cpp

--- printTiles.cpp
+++ printTiles.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //print updated chessboard
 void printTiles(int chessboard[4][4], int score){
-	cout << "=====================" << endl;
+	cout << "---------------------" << endl;
 	for (int i = 0; i < 4; i++){
 		for (int j = 0; j < 4; j++){
 			if (chessboard[i][j] == 0)
@@ -13,8 +13,8 @@ void printTiles(int chessboard[4][4], int score){
 				cout << "|" << setw(4) << chessboard[i][j];
 		}
 		cout << "|" << endl;
+    cout << "---------------------" << endl;
 	}
 
-	cout << "=====================" << endl;
 	cout << "Current Score: " << score << endl;
 }
