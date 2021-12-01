2be3dbc45e2c1bd6b29f2dbc3970218290adb23f
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-12-01 16:04:48 +0800

    Update saveGame.cpp

--- saveGame.cpp
+++ saveGame.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void saveGame(int chessboard[4][4], int score){
 	//open a txt file and save our two-dimentional array
 	ofstream outfile;
-	outfile.open("memory.txt", ios::in);
+	outfile.open("memory.txt");
 	for (int i = 0; i < 4; i++){
 		for (int j=0; j<4; j++){
 			outfile << chessboard[i][j] << "\n";
