f20f957116db46c19411fdd9ccbb2ae5efa7c57b
sdfvj <91207470+sdfvj@users.noreply.github.com>
2021-12-01 23:56:59 +0800

    Update loadGame.cpp

--- loadGame.cpp
+++ loadGame.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void loadGame(int chessboard[4][4], int &score){
   //open the file and read our previous 4*4 array
-    ifstream OpenFile("memory.txt");  
+    ifstream OpenFile("memory.txt",ios::in | ios::binary );  
 　　if (OpenFile.fail())   
      {  
        cout<<"No such a record. PLease start a new game. "<<endl;  
@@ -16,7 +16,7 @@ void loadGame(int chessboard[4][4], int &score){
     for (int i=0; i<4; i++)
     {
       for (int j=0; j<4; j++)        
-         OpenFile>>chessboard[i][j];  
+         OpenFile >> chessboard[i][j];  
     }
     OpenFile.close();  
 
