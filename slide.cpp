ad8d632714afaa45756012680b05b20f92040759
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-12-02 13:53:03 +0800

    Update slide.cpp
    Added header for slide.h

--- slide.cpp
+++ slide.cpp
@@ -1,75 +1,13 @@
-//slide.cpp
+#include "slide.h"
 
 #include <iostream>
+#include <stdlib.h>
+#include <iomanip>
 #include <string>
-//#include "slide.h"
 using namespace std;
 
-//Merge two or more tiles if possible and return the incresed score.
-int merge(int tiles[4][4], char direction){
-  int score = 0;
-  switch (direction){
-    case 'w':
-    case 'W':
-    // Merging upward
-      for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (tiles[j][i] == tiles[j+1][i]) {
-            tiles[j][i] = tiles[j][i]*2;
-            score += tiles[j][i]*2;
-            tiles[j+1][i] = 0;
-          }
-        }
-      }
-      break;
-
-    case 'a':
-    case 'A':
-    // Merging leftward
-    for (int j = 0; j < 4; j++) {
-      for (int i = 0; i < 3; i++) {
-        if (tiles[j][i] == tiles[j][i+1]) {
-          tiles[j][i] = tiles[j][i]*2;
-          score += tiles[j][i]*2;
-          tiles[j][i+1] = 0;
-        }
-      }
-    }
-      break;
-
-    case 's':
-    case 'S':
-    // Merging downward
-      for (int i = 0; i < 4; i++) {
-        for (int j = 3; j > 0; j--) {
-          if (tiles[j][i] == tiles[j-1][i]) {
-            tiles[j][i] = tiles[j][i]*2;
-            score += tiles[j][i]*2;
-            tiles[j-1][i] = 0;
-          }
-        }
-      }
-      break;
-
-    case 'd':
-    case 'D':
-    // Merging rightward
-      for (int j = 0; j < 4; j++) {
-        for (int i = 3; i > 0; i--) {
-          if (tiles[j][i] == tiles[j][i-1]) {
-            tiles[j][i] = tiles[j][i]*2;
-            score += tiles[j][i]*2;
-            tiles[j][i-1] = 0;
-          }
-        }
-      }
-      break;
-  }
-  return score;
-}
-
 //Read user input and slide the tiles.
-void move(int tiles[4][4], char direction){
+void slide(int tiles[4][4], char direction){
   switch (direction){
     case 'w':
     case 'W':
@@ -140,16 +78,4 @@ void move(int tiles[4][4], char direction){
       break;
   }
   return;
-}
-
-bool slide(int tiles[4][4], int &score){
-  char input;
-  cin >> input;
-  if (input != 'w' || input != 'W' || input != 'a' || input != 'A' || input != 's' || input != 'S' || input != 'd' || input != 'D'){
-    return false;
-  }
-  move(tiles, input);
-  score += merge(tiles, input);
-  move(tiles, input);
-  return true;
 }
