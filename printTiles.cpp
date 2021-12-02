9c2023b88b8ab399c8f55e04024be79b31b3a1e1
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-12-02 18:38:06 +0800

    Update printTiles.cpp
    Fixed the problem of unstructured grid when the score is larger than 8192

--- printTiles.cpp
+++ printTiles.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 //Print updated chessboard of a new turn.
 void printTiles(int chessboard[4][4], int score){
-	cout << "---------------------" << endl;
+	cout << "---------------------------------" << endl;
 	for (int i = 0; i < 4; i++){
+		cout << "|" << setw(7) << ' ' << "|" << setw(7) << ' ' << "|" << setw(7) << ' ' << "|" << setw(7) << ' ' << "|" << endl;
 		for (int j = 0; j < 4; j++){
 			if (chessboard[i][j] == 0)
-				cout << "|" << setw(4) << ' '; //Print a blank space if the tiles has value 0
+				cout << "|" << setw(7) << ' '; //Print a blank space if the tiles has value 0
 			else
-				cout << "|" << setw(4) << chessboard[i][j];
+				cout << "|" << setw(7) << chessboard[i][j];
 		}
 		cout << "|" << endl;
-    cout << "---------------------" << endl;
+		cout << "|" << setw(7) << ' ' << "|" << setw(7) << ' ' << "|" << setw(7) << ' ' << "|" << setw(7) << ' ' << "|" << endl;
+    cout << "---------------------------------" << endl;
 	}
 	cout << "Current Score: " << score << endl;
+	cout << endl;
 }
