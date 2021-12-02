db16d441fadbbb70cc789d894481322bb899edfd
Yiyi <90244575+Tekrondoo@users.noreply.github.com>
2021-12-02 11:52:13 +0800

    Update loadGame.cpp
    Fixed Indentations;
    Also retrieve information about the score;
    Add a global variable.

--- loadGame.cpp
+++ loadGame.cpp
@@ -5,21 +5,22 @@ using namespace std;
 
 
 void loadGame(int chessboard[4][4], int &score){
-  //open the file and read our previous 4*4 array
-    ifstream OpenFile("memory.txt",ios::in | ios::binary );  
-　　if (OpenFile.fail())   
-     {  
-       cout<<"No such a record. PLease start a new game. "<<endl;  
-       exit(0);  
-     }  
-    
-    for (int i=0; i<4; i++)
-    {
-      for (int j=0; j<4; j++)        
-         OpenFile >> chessboard[i][j];  
-    }
-    OpenFile.close();  
-
- }
-
- 
+	//open the file and read our previous 4*4 array
+	ifstream OpenFile;
+	OpenFIle.open("memory.txt");  
+	if (OpenFile.fail()){
+		cout<<"Error in file opening! PLease start a new game. "<<endl;
+		fileError = true;
+		return;  
+	}
+  fileError = false;
+	for (int i = 0; i < 4; i++){
+		for (int j = 0; j < 4; j++){
+			OpenFile >> chessboard[i][j];
+		}
+	}
+  OpenFile >> score;
+	OpenFile.close();
+	cout << "Game retrived from previous record." << endl;
+	return;
+}
