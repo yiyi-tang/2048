ddc350c101a7d782b4d0b70ffa272f31c9810f3b
sdfvj <91207470+sdfvj@users.noreply.github.com>
2021-12-03 11:51:07 +0800

    Update equal.cpp

--- equal.cpp
+++ equal.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 
 //Compare if the tiles after move are the same to the previous one.
+//we pass the previous 2 dimentional array and the current nodes as inputs.
+//if the number corresponding to any same position of the 4*4 array is different, there should be a change, hence retunring false.
+//if the number corresponding to every position of the 4*4 array is the same, there should be no changes, hence retunring true.
 bool equal(int tile[4][4], Node * head[4][4]){
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
