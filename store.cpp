300fdd188eaf1f020d14f326bf76129850dcfc0a
sdfvj <91207470+sdfvj@users.noreply.github.com>
2021-12-03 20:26:08 +0800

    Update store.cpp
    complement the program and the comments.

--- store.cpp
+++ store.cpp
@@ -7,6 +7,8 @@
 #include <string>
 using namespace std;
 
+////Take the 4*4 array, score and the nodes as the input.
+//No output.
 //Store the situation of the tiles of every move for undo purpose.
 void store(Node * head[4][4], Node * & scores, int tiles[4][4], int score){
   for (int i = 0; i < 4; i++) {
