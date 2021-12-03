20c2c4844e382154d7532ff0f226b796fd0de17c
sdfvj <91207470+sdfvj@users.noreply.github.com>
2021-12-03 12:20:23 +0800

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -16,59 +16,7 @@ void random(int chessboard[4][4]){
 	chessboard[x][y] = 2 * (rand() % 2 + 1);
 }
 
-void MoveLeft()
-{
-    for (int i = 0; i < 4; i++)  //一共有4行
-    {
-        for (int k = 0, j = 1; j < 4; j++)   //每一行都是有4列（个数字）
-        {
-            if (Bound[i][j] > 0)   //在一行中，只判k只有遇到的第一个非0的个数字  （j>0）
-            {
-                if(Bound[i][k] == Bound[i][j]) //情况一:k == j  &&  j >0
-                {
-                    Score +=  Bound[i][k++] *= 2;
-                    Bound[i][j] = 0;
- 
-                    RandNum_nFalge = 1;
-                }
-                else if (Bound[i][k] == 0)     //情况二:k == 0  && j>0
-                {
-                    Bound[i][k] = Bound[i][j];
-                    Bound[i][j] = 0;
-                    RandNum_nFalge = 1;
-                }
-                else                           //情况三:k != j &&j >0
-                {
-                    Bound[i][++k] = Bound[i][j];
-                    if (k != j)
-                    {
-                        Bound[i][j] = 0;
-                        RandNum_nFalge = 1;
-                    }
- 
-                }
- 
-            }
-        }
-    }
-}
- 
-//右移动
-void MoveRight()
-{
-    for (int i = 0; i < 4; i++)  //一共有4行
-    {
-        for (int k = 3, j = 2; j >= 0; j--)   //每一行都是有4列（个数字）
-        {
-            if (Bound[i][j] > 0)   //在一行中，只判k只有遇到的第一个非0的个数字  （j>0）
-            {
-                if (Bound[i][k] == Bound[i][j]) //情况一:k == j  &&  j >0
-                {
-                    Score += Bound[i][k--] *= 2;
-                    Bound[i][j] = 0;
- 
-                    RandNum_nFalge = 1;
-                }
+
                 else if (Bound[i][k] == 0)     //情况二:k == 0  && j>0
                 {
                     Bound[i][k] = Bound[i][j];
