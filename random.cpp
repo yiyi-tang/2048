b9a72e943c02cf5376bac22f0d36e5190aadc6c2
sdfvj <91207470+sdfvj@users.noreply.github.com>
2021-12-03 18:25:02 +0800

    Update random.cpp
    complement the program and comments.

--- random.cpp
+++ random.cpp
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+//Take the 4*4 array as the input.
+//No output. 
 //Generate one random tile of 2 or 4 in the blank space.
+
 void random(int chessboard[4][4]){
   //for random position
 	srand(time(NULL));
